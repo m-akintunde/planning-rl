0787a623820ccd3a59750de1643030ebd513d104
Michael Akintunde <michael@NMESs-MacBook-Pro.local>
2023-01-13 16:56:52 +0000

    fix: missing states in output plan

--- agents/agent.py
+++ agents/agent.py
@@ -28,13 +28,14 @@ class Agent:
             action = self.choosePolicyAction()
             # append trace
             self.states[-1] = (self.states[-1][0], action)
-            self.states.append((self.State.nxtPolicyPosition(action), "*"))
+            # self.states.append((self.State.nxtPolicyPosition(action), "*"))
             print("current position {} action {}".format(self.State.state, action))
             # by taking the action, it reaches the next state
             self.State = self.takeAction(action)
             # mark is end
             self.State.isEndFunc()
             print("nxt state", self.State.state)
+            self.states.append((self.State.state, "*"))
             print("cost", self.cm[pair_to_int(*self.State.state)])
             print("---------------------")
         return self.states
